1bfbda8fa5a9c175e73a36df3c2f8ce85ae7ffb5
Randymi <rndymi@gmail.com>
2026-08-25 22:19:37 +0200

    Add Core web and operational platform rndymi/piscinapp#5

---
+++ src/main/java/com/rndymi/es/piscinapp/core/platform/openapi/OpenApiConfiguration.java
@@ -0,0 +1,25 @@
+package com.rndymi.es.piscinapp.core.platform.openapi;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "PiscinApp Core API",
+                version = "v0"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfiguration {
+}
