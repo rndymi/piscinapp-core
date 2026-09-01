082a4c2dced185cb5adf087e114f23cacfdd01ad
Randymi <rndymi@gmail.com>
2026-09-02 00:09:21 +0200

    Establish crew persistence model rndymi/piscinapp#15

---
+++ src/main/java/com/rndymi/es/piscinapp/core/crews/persistence/CrewMembershipRepository.java
@@ -0,0 +1,34 @@
+package com.rndymi.es.piscinapp.core.crews.persistence;
+
+import com.rndymi.es.piscinapp.core.crews.domain.CrewMembership;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CrewMembershipRepository
+        extends JpaRepository<CrewMembership, UUID> {
+
+    boolean existsByCrewIdAndEmployeeId(
+            UUID crewId,
+            UUID employeeId
+    );
+
+    Optional<CrewMembership>
+    findByCrewIdAndEmployeeId(
+            UUID crewId,
+            UUID employeeId
+    );
+
+    List<CrewMembership>
+    findAllByCrewId(
+            UUID crewId
+    );
+
+    List<CrewMembership>
+    findAllByCrewIdIn(
+            Collection<UUID> crewIds
+    );
+}
